e9ff2190aa08ce9b33eace1aa79e14587fbc6bd9
SZF1 A1 <szf1 a1@oktatas.local>
2022-03-29 14:36:51 +0200

    Mappa szerkezet

---
+++ inc/config.php
@@ -0,0 +1,3 @@
+<?php
+$name = "Fenntarthatósági témahét";
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require('inc/config.php');
+require('inc/db.php');
+require('inc/functions.php');
+
+switch($_GET['page'])
+{
+	case 'ajax': include("assets/ajax/ajax.php"); break;
+	case 'home': include("pages/home.php"); break;
+	default: include("pages/home.php"); break;
+}
+
+?>
