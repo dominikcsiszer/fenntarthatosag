552100d640069f8a01b589c6ec81a58597516866
cdominik12 <dominikcsiszer@icloud.com>
2022-04-01 19:27:25 +0200

    Quiz kezdet

---
+++ pages/quiz.php
@@ -0,0 +1,36 @@
+<?php 
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?php echo $name ?></title>
+    
+    
+    <!-- Boostrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <script src="assets/js/bootstrap.js"></script>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header"><h1>Quiz</h1></div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/js/jquery-3.1.0.min.js"></script>
+    <script src="assets/ajax/functions.js"></script>
+</body>
+</html>
